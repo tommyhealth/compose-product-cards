ee61f2f56153cc7ffeb6928bea2df97dac1bfdaa
dliza <kr_lizaa@mail.ru>
2026-04-16 22:10:50 +0300

    fix CompareCheckbox

--- app/src/main/java/com/tommyhealth/composecards/presentation/compose/CompareCheckbox.kt
+++ app/src/main/java/com/tommyhealth/composecards/presentation/compose/CompareCheckbox.kt
@@ -17,11 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.res.stringResource
 import com.tommyhealth.composecards.R
 import com.tommyhealth.composecards.ui.theme.ComposeProductCardsTheme
@@ -35,14 +34,24 @@ fun CompareCheckbox(
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
+            .clip(RoundedCornerShape(6.dp))
             .clickable(onClick = onCheckedChange, role = Role.Checkbox)
-            .border(0.7.dp, Color.LightGray, RoundedCornerShape(6.dp))
+            .border(
+                width = 0.7.dp,
+                color = MaterialTheme.colorScheme.outlineVariant,
+                shape = RoundedCornerShape(6.dp)
+            )
             .padding(horizontal = 4.dp, vertical = 5.dp)
     ) {
         Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier
                 .size(18.dp)
-                .border(0.7.dp, Color.LightGray, RoundedCornerShape(4.dp)),
+                .border(
+                    width = 0.7.dp,
+                    color = MaterialTheme.colorScheme.outline,
+                    shape = RoundedCornerShape(4.dp)
+                ),
         ) {
             if (checked) {
                 Icon(
@@ -55,7 +64,7 @@ fun CompareCheckbox(
         Spacer(Modifier.width(4.dp))
         Text(
             text = stringResource(R.string.compare),
-            fontSize = 15.sp,
+            style = MaterialTheme.typography.bodyLarge,
             color = MaterialTheme.colorScheme.onBackground,
         )
     }
